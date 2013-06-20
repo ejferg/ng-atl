de51c1d5fb2c5facffe719ac4b834d6b6a66bae9
OpenShift guest <ejferg@gmail.com>
2013-06-20 03:34:19 -0400

    Setup rtcdatachannel

--- public/js/services/rtc.js
+++ public/js/services/rtc.js
@@ -2,10 +2,13 @@ atl.factory('rtc',
     ['$rootScope', '$q', '$log', '$window', 
     function($rootScope, $q, $log, $window){
 
-        var pc;
-        var connect = function() {
+        var localPeerConn;
+        var remotePeerConn;
+        
+        var connect = function(server, options) {
             
-            pc = new RTCPeerConnection(null);
+            localPeerConn = new RTCPeerConnection(server, options);
+            remotePeerConn = new RTCPeerConnection(server, options);
         };
         
         var getMedia = function(options) {
@@ -15,8 +18,8 @@ atl.factory('rtc',
             $window.getUserMedia(options, 
                 function onSuccess(stream) {
                 
-                    pc.addStream(stream);
-                    pc.createOffer(onOfferCreated);
+                    localPeerConn.addStream(stream);
+                    localPeerConn.createOffer(onOfferCreated);
                     
                     var url = URL.createObjectURL(stream);
                     
@@ -48,20 +51,99 @@ atl.factory('rtc',
             return d.promise;
         };
         
+        var addChannelListeners = function(channel) {
+            
+            channel.onmessage = function onMessageReceived(e) {
+                $log.log('received a message:', e.data);
+            };
+        
+            channel.onopen = function onChannelOpen() {
+                $log.log('rtc :: channel open ::');
+            };
+            
+            channel.onclose = function onChannelClose(e) {
+                $log.error(e);
+            };
+            channel.onerror = function onError(e) {
+                $log.error(e);
+            };
+        };
+        
+        var createChannel = function(options) {
+
+            var d = $q.defer();
+            var type = options.type;
+            var sdp = options.sdp;
+            var defaultPeerConn, attachedPeerConn;
+            
+            if(type == 'local') {
+                
+                defaultPeerConn = localPeerConn;
+                attachedPeerConn = remotePeerConn;
+                
+            } else {
+                
+                defaultPeerConn = remotePeerConn;
+                attachedPeerConn = localPeerConn;
+            }
+            
+            var dataConn = defaultPeerConn.createDataChannel("ngApp", {reliable: false});
+            addChannelListeners(dataConn);
+            
+            defaultPeerConn.onicecandidate = function onIceCandidateReady(e) {
+                
+                if (!e || !e.candidate) return;
+                attachedPeerConn && attachedPeerConn.addIceCandidate(e.candidate);
+            };
+            
+            if(type == 'local') {
+                
+                defaultPeerConn.createOffer(function onOfferCreated(desc) {
+                
+                    defaultPeerConn.setLocalDescription(desc);
+                    return d.resolve({peerConn: defaultPeerConn, desc: desc});
+                });
+                
+            } else {
+                
+                defaultPeerConn.setRemoteDescription(sdp);
+                
+                defaultPeerConn.createAnswer(function onAnswerCreated(desc) {
+                
+                    defaultPeerConn.setLocalDescription(desc);
+                    attachedPeerConn.setRemoteDescription(desc);
+
+                    return d.resolve({peerConn: defaultPeerConn, desc: desc});
+                });
+            }
+            
+            return d.promise;
+        };
+        
+        var addDataChannel = function() {
+            
+            return createChannel({type: 'local'}).then(function(data){
+                    
+                    return createChannel({type: 'remote', sdp: data.desc});
+            });
+        };
+        
+        
         var onAddedRemoteStream = function(e) {
             
         };
         
         var onOfferCreated = function(desc) {
             
-            pc.setLocalDescription(desc);
+            localPeerConn.setLocalDescription(desc);
             
         };
         
         connect();
         
         return {
-            addStream: addStream
+            addStream: addStream,
+            addDataChannel: addDataChannel
         }
     
 }]);
